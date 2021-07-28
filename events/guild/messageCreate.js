255feef1eacf8938b766361e1b8fa01afbd98b99
CarelessInternet <myinternetisg00d@gmail.com>
2021-07-28 19:31:37 +0200

    added rest needed

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -12,12 +12,31 @@ function message(client, Discord, prefix, msg) {
   const cmd = args.shift().toLowerCase();
   const command = client.commands.get(cmd);
 
+  if (!command && !isMusicCommand(cmd)) return;
   const hasCooldown = cooldown(msg, Discord, cmd, command);
   if (hasCooldown) return msg.reply(hasCooldown).catch(console.error);
 
+  // switch statement for commands which we have to give special parameters for
+  switch (cmd) {
+    case 'rickroll':
+      command.execute(msg, args);
+      return client.commands.get('music').execute(msg, ['rick astley never gonna give you up'], 'play', true);
+    case 'amogus':
+      command.execute(msg, args);
+      return client.commands.get('music').execute(msg, ['among us drip theme song original'], 'play', true);
+    case 'desc':
+      return command.execute(msg, args, isMusicCommand);
+    case 'music':
+      return;
+    default:
+      break;
+  }
+
   // if the command exists in the commands folder, run it
-  if (command) command.execute(msg, args, Discord, prefix);
+  if (isMusicCommand(cmd)) return client.commands.get('music').execute(msg, args, cmd);
+  else if (command) command.execute(msg, args, Discord, prefix);
 }
+
 function cooldown(msg, Discord, cmd, command) {
   if (!cooldowns.has(cmd)) cooldowns.set(cmd, new Discord.Collection());
 
@@ -36,6 +55,10 @@ function cooldown(msg, Discord, cmd, command) {
 
   timestamps.set(msg.author.id, currentTime);
   setTimeout(() => timestamps.delete(msg.author.id), amount);
+}
+
+function isMusicCommand(cmd) {
+  return cmd === 'play' || cmd === 'leave' || cmd === 'skip' || cmd === 'queue' || cmd === 'pause' || cmd === 'resume' || cmd === 'unpause' || cmd === 'volume' || cmd === 'loop' || cmd === 'unloop' || cmd === 'remove';
 }
 
 module.exports = message;
