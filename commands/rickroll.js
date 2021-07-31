b3bb54ea2241ffdc64f8e7dcb2c430f8e0c3af83
CarelessInternet <myinternetisg00d@gmail.com>
2021-07-31 19:13:36 +0200

    added rickroll

---
+++ commands/rickroll.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'rickroll',
+  description: 'commits rick roll. joins vc if the message author is in one and plays rick roll',
+  async execute(msg, args) {
+    await msg.channel.send('https://www.youtube.com/watch?v=dQw4w9WgXcQ').catch(console.error);
+  }
+};
