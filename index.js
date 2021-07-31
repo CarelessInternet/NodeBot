6595ed27da49f281ca4627df0ee747797fb05f5f
CarelessInternet <myinternetisg00d@gmail.com>
2021-07-31 17:49:58 +0200

    top.gg server count and shard count this is only for production, not for development

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
 // sharding is required for multiple instances of the bot running at the same time for maximum efficiency and performance
+// uncommented lines are for production where you can see server count and shard count in top.gg
 const {ShardingManager} = require('discord.js');
+// const {AutoPoster} = require('topgg-autoposter');
 const dateFormat = require('dateformat');
 const token = process.env.token;
+// const topGGToken = process.env.topGGToken;
 
 const shards = new ShardingManager('./bot.js', {
   token: token,
@@ -12,5 +15,8 @@ const shards = new ShardingManager('./bot.js', {
 shards.on('shardCreate', shard => {
   console.log(`Created shard #${shard.id} at ${dateFormat(new Date(), 'yyyy-mm-dd HH:MM:ss')}`);
 });
+
+// const poster = AutoPoster(topGGToken, shards);
+// poster.on('error', console.error);
 
 shards.spawn(shards.totalShards);
