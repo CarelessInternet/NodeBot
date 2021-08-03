1ade2a2a37ac452d5ea3acc376f31130c15affb2
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-03 18:47:35 +0200

    updated stuff

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 async function message(client, Discord, prefix, msg) {
   if (!client.application?.owner) await client.application?.fetch();
 
-  if (msg.content.toLowerCase() === '..deploy' && msg.author.id === client.application?.owner.id) {
+  if (msg.content.toLowerCase() === 'nb!deploy' && msg.author.id === client.application?.owner.id) {
     try {
       const data = await JSON.parse(fs.readFileSync('./txt/data.json', 'utf8'));
       await client.application?.commands.set(data);
