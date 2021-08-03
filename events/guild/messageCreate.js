cdcdbe301d6707eb8ca2e9ca61c31dd968a68a49
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-03 18:30:35 +0200

    added try catch

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -4,10 +4,14 @@ async function message(client, Discord, prefix, msg) {
   if (!client.application?.owner) await client.application?.fetch();
 
   if (msg.content.toLowerCase() === '..deploy' && msg.author.id === client.application?.owner.id) {
-    const data = await JSON.parse(fs.readFileSync('./txt/data.json', 'utf8'));
-    await client.application?.commands.set(data);
-    
-    console.log('Successfully deployed commands!');
+    try {
+      const data = await JSON.parse(fs.readFileSync('./txt/data.json', 'utf8'));
+      await client.application?.commands.set(data);
+  
+      console.log('Successfully deployed commands!');
+    } catch(err) {
+      console.error(err);
+    }
   }
 }
 
