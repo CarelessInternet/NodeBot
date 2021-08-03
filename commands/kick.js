e7d58d2d0c372046efe19b67bc9437af92a51a59
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-03 23:00:32 +0200

    added kick command

---
+++ commands/kick.js
@@ -0,0 +1,61 @@
+const {MessageActionRow, MessageButton} = require('discord.js');
+
+module.exports = {
+  name: 'kick',
+  async execute(interaction) {
+    const args = [interaction.options.get('user')?.member, interaction.options.get('reason')?.value];
+    if (!args[0]) interaction.reply({content: 'Please specify a user', ephemeral: true}).catch(console.error);
+    if (!interaction.guild.me.permissions.has(['KICK_MEMBERS', 'MANAGE_MESSAGES'])) return interaction.reply({content: 'I need the kick members and manage messages permissions to run this command', ephemeral: true}).catch(console.error);
+    if (!interaction.member.permissions.has('KICK_MEMBERS')) return interaction.reply({content: 'You do not have the right permissions to kick a member', ephemeral: true}).catch(console.error);
+
+    const filter = i => (i.customId === 'Confirm' || i.customId === 'Abort') && i.member.user.id === interaction.member.id;
+    const reason = args[1] ?? '';
+    const row = new MessageActionRow()
+    .addComponents(
+      new MessageButton()
+      .setCustomId('Confirm')
+      .setEmoji('✔️')
+      .setStyle('SUCCESS'),
+      new MessageButton()
+      .setCustomId('Abort')
+      .setEmoji('❌')
+      .setStyle('DANGER')
+    );
+    const confirmation = await interaction.reply({
+      content: `Are you sure you want to kick <@${args[0].user.id}>${reason ? ' for the following reason: ' + reason : ''}?`,
+      components: [row],
+      fetchReply: true
+    }).catch(console.error);
+    const collector = confirmation.createMessageComponentCollector({filter, max: 1, time: 7 * 1000});
+
+    collector.on('collect', i => {
+      const reaction = i.customId;
+      if (reaction === 'Confirm') {
+        if (args[0].user.id === interaction.member.id) return i.update({content: 'You cannot kick yourself', components: []}).catch(console.error);
+        if (args[0].permissions.has('KICK_MEMBERS')) return i.update({content: 'Failed to kick because the user has kick members permission', components: []}).catch(console.error);
+
+        args[0].kick(reason)
+        .then(user => i.update({content: `👍 ${user.id ? '<@' + user.id + '>' : user.user.username} has been kicked from ${interaction.guild.name}`, components: []}).catch(console.error))
+        .catch(err => i.update({content: '👎 Failed to kick, usually because the user has some form of mod/admin on this server, or my highest role is listed lower than the requested user\'s roles', components: []}).catch(console.error));
+      } else if (reaction === 'Abort') {
+        i.update({content: 'Kick has been aborted', components: []}).catch(console.error);
+      }
+    });
+    collector.on('end', (collected, reason) => {
+      switch (reason) {
+        case 'time':
+          return confirmation.edit({content: 'Kick aborted due to no response', components: []}).catch(console.error);
+        case 'messageDelete':
+          return interaction.channel.send({content: 'Kick aborted because the message was deleted'}).catch(console.error);
+        case 'channelDelete':
+          return;
+        case 'guildDelete':
+          return;
+        case 'limit':
+          return;
+        default:
+          return interaction.channel.send({content: 'Kick aborted due to an unknown reason'}).catch(console.error);
+      }
+    });
+  }
+}
