923289e5374d10e34a8a377335ea3e22af52514d
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-06 16:29:48 +0200

    updated say command

--- commands/say.js
+++ commands/say.js
@@ -4,6 +4,9 @@ module.exports = {
     const arg = interaction.options.get('text')?.value;
     if (interaction.inGuild() && !interaction.member.permissions.has('MANAGE_CHANNELS')) return interaction.reply({content: 'You need the manage channels persmission to run this command', ephemeral: true}).catch(console.error);
 
-    interaction.reply({content: arg}).catch(console.error);
+    interaction.reply({content: 'Sending...', ephemeral: true}).catch(console.error);
+    interaction.channel.send({content: arg})
+    .then(msg => interaction.editReply({content: 'Sent!'}))
+    .catch(console.error);
   }
 }
