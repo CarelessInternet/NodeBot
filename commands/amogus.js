ccb21273900332fc22be244a97321f4162abf8dc
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-06 02:38:16 +0200

    if not instanceof message dont react

--- commands/amogus.js
+++ commands/amogus.js
@@ -1,3 +1,5 @@
+const {Message} = require('discord.js');
+
 module.exports = {
   name: 'amogus',
   async execute(interaction) {
@@ -27,12 +29,13 @@ module.exports = {
     `;
     
     try {
-      const msg = await interaction.reply({
+      const message = await interaction.reply({
         content: text,
         fetchReply: true
       });
-      const channel = await interaction.client.channels.fetch(interaction.channelId);
-      const message = await channel.messages.fetch(msg.id);
+      // cant declare const something = new Message(interaction.client, message) because for some reason it cant do methods like react or edit
+      // so i have to stop the reacting if the message variable returns APIMessage and not Message
+      if (!(message instanceof Message)) return;
 
       message.react('😳');
       message.react('😮');
