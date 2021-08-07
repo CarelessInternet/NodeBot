41eedf5fc79a666ad8e2070e2c3b049d205c2785
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-07 12:33:42 +0200

    changed to only allow in guilds

--- commands/say.js
+++ commands/say.js
@@ -1,12 +1,13 @@
 module.exports = {
   name: 'say',
-  execute(interaction) {
+  async execute(interaction) {
+    if (!interaction.inGuild()) return interaction.reply({content: 'You must be in a guild to use this command'}).catch(console.error);
+    if (!interaction.member.permissions.has('MANAGE_CHANNELS')) return interaction.reply({content: 'You need the manage channels persmission to run this command', ephemeral: true}).catch(console.error);
     const arg = interaction.options.get('text')?.value;
-    if (interaction.inGuild() && !interaction.member.permissions.has('MANAGE_CHANNELS')) return interaction.reply({content: 'You need the manage channels persmission to run this command', ephemeral: true}).catch(console.error);
 
-    interaction.reply({content: 'Sending...', ephemeral: true}).catch(console.error);
+    await interaction.reply({content: 'Sending...', ephemeral: true}).catch(console.error);
     interaction.channel.send({content: arg})
-    .then(msg => interaction.editReply({content: 'Sent!'}))
+    .then(msg => interaction.editReply({content: 'Sent!'}).catch(console.error))
     .catch(console.error);
   }
 }
