df59b769a372accb2ddabf260f3dd7b291cf6c0d
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-13 17:55:21 +0200

    rewrote users.js to make it easier to read

--- commands/users.js
+++ commands/users.js
@@ -2,14 +2,23 @@ module.exports = {
   name: 'users',
   execute(interaction) {
     if (!interaction.inGuild()) return interaction.reply({content: 'This command is only available in a server'}).catch(console.error);
-    const arg = interaction.options.get('role')?.value;
-
-    if (arg) {
-      const amount = interaction.guild.roles.cache.get(arg).members.map(member => !member.user.bot).length;
-      interaction.reply({content: `${amount} ${amount == 1 ? 'user' : 'users'} in the server with the <@&${arg}> role`}).catch(console.error);
-    } else {
-      const amount = interaction.guild.members.cache.filter(member => !member.user.bot).size;
-      interaction.reply({content: `${amount} ${amount == 1 ? 'user' : 'users'} in this server`}).catch(console.error);
+    const role = interaction.options.get('role')?.role;
+    
+    try {
+      if (role) {
+        if (role.name === '@everyone') return interaction.reply({content: 'You must choose a valid role', ephemeral: true});
+        const amount = role.members.map(member => !member.user.bot).length;
+        interaction.reply({content: `${amount} ${amount === 1 ? 'user' : 'users'} in the server with the <@&${role.id}> role`});
+      } else {
+        const amount = interaction.guild.members.cache.filter(member => !member.user.bot).size;
+        interaction.reply({content: `${amount} ${amount === 1 ? 'user' : 'users'} in this server`});
+      }
+    } catch(err) {
+      console.error(err);
+      interaction.reply({
+        content: 'An unknown error occured, please try again later',
+        ephemeral: true
+      }).catch(console.error);
     }
   }
 }
