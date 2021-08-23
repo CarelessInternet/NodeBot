dabac58cd2ce834b98cd116af25af0718b004dbd
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-23 21:37:14 +0200

    increased performance by changing encryption and searching

--- commands/music.js
+++ commands/music.js
@@ -1,62 +1,102 @@
+const search = require('youtube-search');
 const ytdl = require('ytdl-core');
-const ytSearch = require('yt-search');
 const {MessageEmbed} = require('discord.js');
 const {joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus} = require('@discordjs/voice');
 const queue = new Map();
 
-// return undefined if no result, return video if result
-async function videoFinder(query) {
-  const result = await ytSearch(query);
-  return result.videos?.[0];
+// reject if no result, resolve with video if result
+function videoFinder(query) {
+  // the reason why im not using the yt-search npm module is because
+  // it takes a long time to get a response, which causes a small pause in the music
+  // before resuming again, this is for performance reasons
+  return new Promise(async (resolve, reject) => {
+    try {
+      const options = {
+        key: process.env.youtubeAPIKey,
+        type: 'video',
+        maxResults: 1
+      };
+    
+      const result = await search(query, options);
+      if (result.error) reject(result.error.message);
+      if (!result.results[0]) reject('No search result found');
+    
+      const {link, description, title, thumbnails, channelTitle, id} = result.results[0];
+      const {videoDetails} = await ytdl.getBasicInfo(link);
+
+      resolve({
+        title: title,
+        url: link,
+        author: {
+          name: channelTitle,
+          url: videoDetails['channel_url']
+        },
+        description: description,
+        image: thumbnails.high.url,
+        duration: {
+          seconds: parseInt(videoDetails.lengthSeconds),
+          timestamp: `${Math.floor(parseInt(videoDetails.lengthSeconds) / 60)}:${('0' + Math.floor(parseInt(videoDetails.lengthSeconds) % 60)).slice(-2)}`
+        },
+        views: parseInt(videoDetails.viewCount),
+        videoId: id
+      });
+    } catch(err) {
+      reject(err);
+    }
+  });
 }
 // prepares video before calling the video player function
 async function play(interaction, serverQueue, channel, botArg = '') {
-  const arg = interaction.options.getString('video') ?? botArg;
-  const video = await videoFinder(arg);
-  if (!video) return interaction.followUp({content: 'No search result found', ephemeral: true}).catch(console.error);
-  if (video.duration.seconds > 10800) return interaction.followUp({content: 'Video must be less than 3 hours long', ephemeral: true}).catch(console.error);
-
-  if (!serverQueue) {
-    // all necessary information needed for sending messages, setting up loops, queue system, etc
-    const constructor = {
-      queue: [],
-      volume: 1,
-      player: null,
-      resource: null,
-      connection: null,
-      loop: false
-    };
-
-    // add the info into a map/hashmap with the key as the guild id
-    queue.set(interaction.guild.id, constructor);
-    constructor.queue.push(video);
-
-    try {
-      // idk what adapterCreator is but whatever, it's required
-      const connection = await joinVoiceChannel({
-        channelId: channel.id,
-        guildId: channel.guild.id,
-        adapterCreator: channel.guild.voiceAdapterCreator
-      });
-
-      connection.on(VoiceConnectionStatus.Disconnected, () => queue.delete(interaction.guild.id));
-      connection.on(VoiceConnectionStatus.Destroyed, () => {
-        queue.delete(interaction.guild.id);
-        interaction.channel.send({content: '☠️ Disconnected from voice channel'}).catch(console.error);
-      });
-      connection.on('error', console.error);
-      constructor.connection = connection;
-
-      videoPlayer(interaction, constructor);
-    } catch(err) {
-      console.error(err);
-      queue.get(interaction.guild.id).connection.destroy();
-
-      interaction.followUp({content: 'There was an error creating a connection, please try again later', ephemeral: true}).catch(console.error);
+  try {
+    const arg = interaction.options.getString('video') ?? botArg;
+    const video = await videoFinder(arg);
+    if (video.duration?.seconds > 10800) return interaction.followUp({content: 'Video must be less than 3 hours long', ephemeral: true}).catch(console.error);
+  
+    if (!serverQueue) {
+      // all necessary information needed for sending messages, setting up loops, queue system, etc
+      const constructor = {
+        queue: [],
+        volume: 1,
+        player: null,
+        resource: null,
+        connection: null,
+        loop: false
+      };
+  
+      // add the info into a map/hashmap with the key as the guild id
+      queue.set(interaction.guild.id, constructor);
+      constructor.queue.push(video);
+  
+      try {
+        // idk what adapterCreator is but whatever, it's required
+        const connection = await joinVoiceChannel({
+          channelId: channel.id,
+          guildId: channel.guild.id,
+          adapterCreator: channel.guild.voiceAdapterCreator
+        });
+  
+        connection.on(VoiceConnectionStatus.Disconnected, () => queue.delete(interaction.guild.id));
+        connection.on(VoiceConnectionStatus.Destroyed, () => {
+          queue.delete(interaction.guild.id);
+          interaction.channel.send({content: '☠️ Disconnected from voice channel'}).catch(console.error);
+        });
+        connection.on('error', console.error);
+        constructor.connection = connection;
+  
+        videoPlayer(interaction, constructor);
+      } catch(err) {
+        console.error(err);
+        queue.get(interaction.guild.id).connection.destroy();
+  
+        interaction.followUp({content: 'There was an error creating a connection, please try again later', ephemeral: true}).catch(console.error);
+      }
+    } else {
+      serverQueue.queue.push(video);
+      interaction.followUp({content: `👍 Video added to queue: ***${video.title}***, length: **${video.duration.timestamp}**`}).catch(console.error);
     }
-  } else {
-    serverQueue.queue.push(video);
-    interaction.followUp({content: `👍 Video added to queue: ***${video.title}***, length: **${video.duration.timestamp}**`}).catch(console.error);
+  } catch(e) {
+    console.error(e);
+    interaction.followUp({content: e ?? 'No search result found or some other error occured', ephemeral: true}).catch(console.error)
   }
 }
 // the function that actually plays the video
@@ -98,7 +138,6 @@ async function videoPlayer(interaction, constructor) {
     .setURL(songQueue.url)
     .setAuthor(songQueue.author.name)
     .setDescription(songQueue.description)
-    .setThumbnail(songQueue.thumbnail)
     .addFields({
       name: '\u200B',
       value: '\u200B'
