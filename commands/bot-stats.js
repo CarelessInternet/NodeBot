26c9fbdc7350756e2026a63ba1fb2b5158d560f4
CarelessInternet <myinternetisg00d@gmail.com>
2021-08-24 21:13:38 +0200

    added bot-stats command

---
+++ commands/bot-stats.js
@@ -0,0 +1,49 @@
+const {MessageEmbed} = require('discord.js');
+
+module.exports = {
+  name: 'bot-stats',
+  async execute(interaction) {
+    try {
+      const {shard} = interaction.client;
+      const promises = [
+        shard.fetchClientValues('guilds.cache.size'),
+        shard.fetchClientValues('emojis.cache.size'),
+        shard.fetchClientValues('channels.cache.size'),
+        shard.broadcastEval(c => c.guilds.cache.reduce((acc, curr) => acc + curr.memberCount, 0))
+      ];
+
+      const [guildSize, emojiSize, channelSize, memberSize] = await Promise.all(promises);
+      const embed = new MessageEmbed()
+      .setColor('RANDOM')
+      .setAuthor(interaction.user.tag, interaction.user.avatarURL())
+      .setTitle('Bot Stats')
+      .setDescription('Shows stats of the bot, like the amount of guilds')
+      .addFields({
+        name: 'Servers',
+        value: '📊 ' + guildSize.reduce((acc, curr) => acc + curr, 0).toLocaleString(),
+        inline: true
+      }, {
+        name: 'Members',
+        value: '👤 ' + memberSize.reduce((acc, curr) => acc + curr, 0).toLocaleString(),
+        inline: true
+      }, {
+        name: '\u200B',
+        value: '\u200B'
+      }, {
+        name: 'Channels',
+        value: '📺 ' + channelSize.reduce((acc, curr) => acc + curr, 0).toLocaleString(),
+        inline: true
+      }, {
+        name: 'Emojis',
+        value: '💩 ' + emojiSize.reduce((acc, curr) => acc + curr, 0).toLocaleString(),
+        inline: true
+      })
+      .setTimestamp();
+
+      interaction.reply({embeds: [embed]});
+    } catch(err) {
+      console.error(err);
+      interaction.reply({content: 'An unknown error occured whilst fetching data, please try again later', ephemeral: true});
+    }
+  }
+}
