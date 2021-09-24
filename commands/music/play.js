87a46c5c1d145a0ccaba3d57b27ebfff8eaecb5b
CarelessInternet <myinternetisg00d@gmail.com>
2021-09-24 21:55:51 +0200

    rewrote for cleaner code and better practices

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,185 +1,149 @@
-const search = require('youtube-search');
-const secondarySearch = require('yt-search');
-const {getBasicInfo} = require('ytdl-core');
-const ytdl = require('play-dl');
+const ytdl = require('ytdl-core');
+const ytsr = require('ytsr');
 const {MessageEmbed} = require('discord.js');
 const {joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, AudioPlayerStatus, VoiceConnectionStatus} = require('@discordjs/voice');
 const check = require('../../musicCheck');
 
 // reject if no result, resolve with video if result
 function videoFinder(query) {
-  // the reason why im not using the yt-search npm module is because
-  // it takes a long time to get a response, which causes a small pause in the music
-  // before resuming again, this is for performance reasons
   return new Promise(async (resolve, reject) => {
     try {
-      const options = {
-        key: process.env.youtubeAPIKey,
-        type: 'video',
-        maxResults: 1
-      };
-    
-      const result = await search(query, options);
-      if (!result.results[0]) reject('No search result found');
-    
-      const {link, description, title, thumbnails, channelTitle, id} = result.results[0];
-      const {videoDetails} = await getBasicInfo(link);
+      const filters = await ytsr.getFilters(query);
+      const {url} = filters.get('Type').get('Video');
+      const results = await ytsr(url, {pages: 1});
 
-      resolve({
-        title: title,
-        url: link,
-        author: {
-          name: channelTitle,
-          url: videoDetails['author']['channel_url']
-        },
-        description: description,
-        image: thumbnails.high.url,
-        duration: {
-          seconds: parseInt(videoDetails.lengthSeconds),
-          timestamp: `${Math.floor(parseInt(videoDetails.lengthSeconds) / 60)}:${('0' + Math.floor(parseInt(videoDetails.lengthSeconds) % 60)).slice(-2)}`
-        },
-        views: parseInt(videoDetails.viewCount),
-        videoId: id
-      });
+      const {items} = results;
+      if (!items[0]) reject('No video was found with that query');
+
+      resolve(items[0]);
     } catch(err) {
-      // just in case the quota is exceeded or an error occures, we'll use the yt-search module
-      const result = await secondarySearch(query).catch(reject);
-      if (!result.videos?.[0]) reject('No search result found');
-
-      resolve(result.videos[0]);
+      reject(err);
     }
   });
+}
+// returns info about video
+function returnVideoInformation(video) {
+  const embed = new MessageEmbed()
+  .setColor('RANDOM')
+  .setTitle(video.title)
+  .setURL(video.url)
+  .setAuthor(video.author.name, video.author.bestAvatar.url, video.author.url)
+  .setDescription(video.description ?? '')
+  .addField('\u200B', '\u200B')
+  .addField('Duration', video.duration, true)
+  .addField('Views', video.views.toLocaleString(), true)
+  .addField('Video ID', video.id, true)
+  .setThumbnail(video.bestThumbnail.url)
+  .setTimestamp();
+
+  return embed;
 }
 // prepares video before calling the video player function
 async function play(interaction, queue, serverQueue, channel, botArg = '') {
   try {
     const arg = interaction.options.getString('video') ?? botArg;
     const video = await videoFinder(arg);
-    if (video.duration?.seconds > 10800) return interaction.followUp({content: 'Video must be less than 3 hours long', ephemeral: true}).catch(console.error);
-  
+
+    // if (duration in seconds is more than 10800 (3 hours))    
+    if (video.duration.split(':').reduce((acc, time) => (60 * acc) + +time, 0) > 10800) return interaction.followUp({content: 'Video must be less than 3 hours long', ephemeral: true});
+
     if (!serverQueue) {
       // all necessary information needed for sending messages, setting up loops, queue system, etc
+      const musicChannel = await interaction.guild.channels.fetch(interaction.channelId);
       const constructor = {
         queue: [],
+        channel: musicChannel,
         volume: 1,
         player: null,
         resource: null,
         connection: null,
         loop: false
       };
-  
-      // add the info into a map/hashmap with the key as the guild id
+
+      // identify each server with it's id
       queue.set(interaction.guildId, constructor);
       constructor.queue.push(video);
-  
-      try {
-        // idk what adapterCreator is but whatever, it's required
-        const connection = await joinVoiceChannel({
-          channelId: channel.id,
-          guildId: channel.guild.id,
-          adapterCreator: channel.guild.voiceAdapterCreator
-        });
-  
-        connection.on(VoiceConnectionStatus.Disconnected, async () => {
-          try {
-            await Promise.race([
-              entersState(connection, VoiceConnectionStatus.Signalling, 5000),
-              entersState(connection, VoiceConnectionStatus.Connecting, 5000)
-            ]);
-          } catch(err) {
-            if (connection.state.status !== VoiceConnectionStatus.Destroyed) connection.destroy();
-          }
-        });
-        connection.on(VoiceConnectionStatus.Destroyed, async () => {
-          queue.delete(interaction.guildId);
-          interaction.channel?.send({content: '☠️ Disconnected from voice channel'}).catch(console.error);
-        });
-        connection.on('error', console.error);
-        constructor.connection = connection;
-  
-        videoPlayer(interaction, constructor);
-      } catch(err) {
-        console.error(err);
-        queue.get(interaction.guildId).connection.destroy();
-  
-        interaction.followUp({content: 'There was an error creating a connection, please try again later', ephemeral: true}).catch(console.error);
-      }
+
+      const connection = joinVoiceChannel({
+        channelId: channel.id,
+        guildId: channel.guild.id,
+        adapterCreator: channel.guild.voiceAdapterCreator
+      });
+
+      connection.on(VoiceConnectionStatus.Disconnected, async () => {
+        try {
+          // if all promises resolve, the client seems to be reconnecting to a new channel, else it seems to be a real disconnect
+          await Promise.race([
+            entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
+            entersState(connection, VoiceConnectionStatus.Connecting, 5_000)
+          ]);
+        } catch(err) {
+          if (connection.state.status !== VoiceConnectionStatus.Destroyed) connection.destroy();
+        }
+      });
+      connection.on(VoiceConnectionStatus.Destroyed, async () => {
+        const {channel: textChannel} = queue.get(interaction.guildId);
+        queue.delete(interaction.guildId);
+
+        textChannel.send({content: '☠️ Disconnected from voice channel'}).catch(console.error);
+      });
+      connection.on('error', console.error);
+
+      constructor.connection = connection;
+      videoPlayer(interaction, constructor);
     } else {
       serverQueue.queue.push(video);
-      interaction.followUp({content: `👍 Video added to queue: ***${video.title}***, length: **${video.duration.timestamp}**`}).catch(console.error);
+
+      const embed = returnVideoInformation(video);
+      interaction.followUp({content: '👍 Video added to queue:', embeds: [embed]});
     }
-  } catch(e) {
-    console.error(e);
-    interaction.followUp({content: e ?? 'No search result found or some other error occured', ephemeral: true}).catch(console.error)
+  } catch(err) {
+    console.error(err);
+    interaction.followUp({content: err ?? 'An unknown error occured, please try again later', ephemeral: true}).catch(console.error);
   }
 }
 // the function that actually plays the video
-async function videoPlayer(interaction, constructor) {
+function videoPlayer(interaction, constructor) {
   const songQueue = constructor.queue[0];
-  const connection = constructor.connection;
+  const {connection, channel} = constructor;
   if (!songQueue) return connection.destroy();
 
   try {
-    // create connection and play it
-    const video = await ytdl.stream(songQueue.url);
-    const resource = createAudioResource(video.stream, {
-      inputType: video.type,
-      inlineVolume: true
-    })
+    // create audio resource & player, and play the video
+    // we have to set quality to lowestaudio due to something about discord audio bitrate or something
+    // more here: https://github.com/fent/node-ytdl-core/issues/994#issuecomment-912891183
+    const video = ytdl(songQueue.url, {filter: 'audioonly', quality: 'lowestaudio', highWaterMark: 1 << 25});
+    const resource = createAudioResource(video, {inlineVolume: true});
     const player = createAudioPlayer();
-  
+
+    // equivalent to an on finish event
+    player.on(AudioPlayerStatus.Idle, () => {
+      // if there is a loop, replay video, else delete video and move on to next one
+      // destructuring loop from constructor doesnt work for some reason
+      if (constructor.loop) return videoPlayer(interaction, constructor);
+
+      constructor.queue.shift();
+      videoPlayer(interaction, constructor);
+    });
+    player.on('error', console.error);
+
     resource.volume.setVolume(constructor.volume);
     player.play(resource);
     connection.subscribe(player);
     constructor.resource = resource;
     constructor.player = player;
-  
-    // equivalent to on finish event
-    player.on(AudioPlayerStatus.Idle, () => {
-      // if there is a loop, replay video, otherwise delete video and move on to next one
-      if (constructor.loop) return videoPlayer(interaction, constructor);
-      constructor.queue.shift();
-      videoPlayer(interaction, constructor);
-    });
-    player.on('error', err => {
-      console.error(err);
-      interaction.followUp({content: 'An unknown error occured whilst creating the audio resource, please try again later'});
-    });
 
     if (!constructor.loop) {
-      const embed = new MessageEmbed()
-      .setColor('RANDOM')
-      .setTitle(songQueue.title)
-      .setURL(songQueue.url)
-      .setAuthor(songQueue.author.name)
-      .setDescription(songQueue.description)
-      .addFields({
-        name: '\u200B',
-        value: '\u200B'
-      },
-      {
-        name: 'Link to Channel',
-        value: `[${songQueue.author.name}](${songQueue.author.url})`,
-        inline: true
-      }, {
-        name: 'Timestamp',
-        value: songQueue.duration.timestamp,
-        inline: true
-      }, {
-        name: 'Views',
-        value: songQueue.views.toLocaleString(),
-        inline: true
-      })
-      .setThumbnail(songQueue.image)
-      .setTimestamp()
-      .setFooter(`Video ID: ${songQueue.videoId}`);
-  
-      interaction.followUp({embeds: [embed]}).catch(() => interaction.channel?.send({embeds: [embed]}).catch(console.error));
+      const embed = returnVideoInformation(songQueue);
+
+      if (!interaction.replied) interaction.followUp({embeds: [embed]});
+      else channel.send({embeds: [embed]});
     }
   } catch(err) {
     console.error(err);
     connection.destroy();
-    interaction.followUp({content: 'An error occured whilst downloading the video, please try again later\n' + err, ephemeral: true}).catch(console.error);
+
+    channel.send({content: 'An unknown error occured while doing something with the music\n' + err}).catch(console.error);
   }
 }
 
