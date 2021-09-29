577695d58300cba2878c42fff7e9d00e77c0556f
CarelessInternet <myinternetisg00d@gmail.com>
2021-09-29 21:03:39 +0200

    added channel count without threads to bot-stats

--- commands/bot-stats.js
+++ commands/bot-stats.js
@@ -14,46 +14,30 @@ module.exports = {
   async execute(interaction) {
     try {
       if (interaction.user.id !== process.env.ownerID) return interaction.reply({content: 'You are not the owner of this bot, you may not view the stats', ephemeral: true});
-
+      
       const {shard} = interaction.client;
       const promises = [
         shard.fetchClientValues('guilds.cache.size'),
         shard.fetchClientValues('emojis.cache.size'),
         shard.fetchClientValues('channels.cache.size'),
         shard.fetchClientValues('users.cache.size'),
+        shard.broadcastEval(c => c.guilds.cache.reduce((acc, curr) => acc + curr.channels.channelCountWithoutThreads, 0)),
         shard.broadcastEval(c => c.guilds.cache.reduce((acc, curr) => acc + curr.memberCount, 0))
       ];
 
-      const [guildSize, emojiSize, channelSize, userSize, memberSize] = await Promise.all(promises);
+      const [guildSize, emojiSize, channelSize, userSize, channelSizeWithoutThreads, memberSize] = await Promise.all(promises);
       const embed = new MessageEmbed()
       .setColor('RANDOM')
       .setAuthor(interaction.user.tag, interaction.user.avatarURL())
       .setTitle('Bot Stats')
-      .setDescription('Shows stats of the bot, like the amount of guilds')
-      .addFields({
-        name: 'Servers',
-        value: '📊 ' + guildSize.reduce((acc, curr) => acc + curr, 0).toLocaleString(),
-        inline: true
-      }, {
-        name: 'Server Members',
-        value: '👥 ' + memberSize.reduce((acc, curr) => acc + curr, 0).toLocaleString(),
-        inline: true
-      }, {
-        name: 'Cached Users',
-        value: '👤 ' + userSize.reduce((acc, curr) => acc + curr, 0).toLocaleString(),
-        inline: true
-      }, {
-        name: '\u200B',
-        value: '\u200B'
-      }, {
-        name: 'Channels',
-        value: '📺 ' + channelSize.reduce((acc, curr) => acc + curr, 0).toLocaleString(),
-        inline: true
-      }, {
-        name: 'Emojis',
-        value: '💩 ' + emojiSize.reduce((acc, curr) => acc + curr, 0).toLocaleString(),
-        inline: true
-      })
+      .setDescription('Shows info about the bot\'s stats')
+      .addField('Servers', '📊 ' + guildSize.reduce((acc, curr) => acc + curr, 0), true)
+      .addField('Server Members', '👥 ' + memberSize.reduce((acc, curr) => acc + curr, 0), true)
+      .addField('Cached Users', '👤 ' + userSize.reduce((acc, curr) => acc + curr, 0), true)
+      .addField('\u200B', '\u200B')
+      .addField('Channels + Threads', '📺 ' + channelSize.reduce((acc, curr) => acc + curr, 0), true)
+      .addField('Channels - Threads', '💻 ' + channelSizeWithoutThreads.reduce((acc, curr) => acc + curr, 0), true)
+      .addField('Emojis', '💩 ' + emojiSize.reduce((acc, curr) => acc + curr, 0), true)
       .setTimestamp();
 
       interaction.reply({embeds: [embed], ephemeral: true});
